0a7efb7faa8aee817e855b883c1f764cb8236a77
Musa <Musaxlilov29@gmail.com>
2025-07-19 18:21:54 +0400

    Some comments added

--- src/components/organisms/MenuItems/MenuItems.tsx
+++ src/components/organisms/MenuItems/MenuItems.tsx
@@ -4,10 +4,19 @@ import styles from './MenuItemsStyles'
 const menuItemsToDisplay = ['Hummus \n Moutabal \n Falafel \n Marinated Olives \n Kofta \n Eggplant \n Salad \n Lentil Burger \n Smoked Salmon \n Kofta Burger \n Turkish Kebab \n Fries \n Buttered Rice \n Bread Sticks \n Pita Pocket \n Lentil Soup \n Greek Salad \n Rice Pilaf \n Baklava \n Tartufo \n Tiramsu \n Pannan Cotta',
 ]
 
+//!=> You can set horizontal={boolean} or vertical={boolean} values to the ScrollView component
+//TODO=> By doing that you can set the scrolling elements be horizontal view or vertical view
+
+//!=> You can set indicatorStyle={'colorName'} to the ScrollView and you can set the color style to the indicator
+
 const MenuItems = () => {
    return(
       <View style={styles.menu_items_wrapper}>
-         <ScrollView style={styles.scroll_view}>
+         <ScrollView 
+         horizontal={false} 
+         style={styles.scroll_view}
+         indicatorStyle={'white'}
+         >
             <Text style={styles.view_menu}>View Menu</Text>
             <Text style={styles.menu_items}>{menuItemsToDisplay[0]}</Text>
          </ScrollView>
