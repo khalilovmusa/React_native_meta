aeb5f2dc9a30d1c1b11d2a7f0a57f435bf9c3126
Musa <Musaxlilov29@gmail.com>
2025-08-04 17:20:40 +0400

    How to render images and image component props

--- src/components/learncomponents/WelcomePageWLogo/index.tsx
+++ src/components/learncomponents/WelcomePageWLogo/index.tsx
@@ -1,17 +1,45 @@
-import { Image, Text, View } from "react-native"
+import { Image, Text, ScrollView } from "react-native"
 import styles from './styles'
 
 const WelcomePageWLogo = () => {
    return(
-      <View style={styles.container}>
+      <ScrollView style={styles.container}>
          <Image 
-          source={require("../../../assets/littleLemonHeader.png")}
-          style={styles.logo}
+            source={require("../../../assets/littleLemonHeader.png")}
+            style={styles.logo}
+            accessible={true}
+            //!=> It is always good to keep accessibility as a priority while developing applications. The accessible prop accepts a Boolean true or false. When it is set to true, it indicates that the image is an accessibility element.
+            accessibilityLabel="Little Lemon Header"
+            //!=> Once the image is set to be accessible, it is time to provide an accessible label for the image. This is the text that the screen reader reads when the user interacts with the image. Make sure to give a meaningful and descriptive name to the prop.
          />
          <Text style={styles.title}>
             Little lemon, your local Mediterranean Bistro
          </Text>
-      </View>
+         <Image 
+            source={require("../../../assets/pic1.png")}
+            style={styles.image}
+            accessible={true}
+            accessibilityLabel="Food picture 1"
+         />
+         <Image 
+            source={require("../../../assets/pic2.png")}
+            style={styles.image}
+            accessible={true}
+            accessibilityLabel="Food picture 2"
+         />
+         <Image 
+            source={require("../../../assets/pic3.png")}
+            style={styles.image}
+            accessible={true}
+            accessibilityLabel="Food picture 3"
+         />
+         <Image 
+            source={require("../../../assets/pic4.png")}
+            style={styles.image}
+            accessible={true}
+            accessibilityLabel="Food picture 4"
+         />
+      </ScrollView>
    )
 }
 
--- src/components/learncomponents/WelcomePageWLogo/styles.ts
+++ src/components/learncomponents/WelcomePageWLogo/styles.ts
@@ -5,6 +5,11 @@ export default StyleSheet.create({
       height: 100,
       width: 300,
       resizeMode: 'contain'
+      //!=> cover:  This is used to scale the image uniformly and maintain the image’s aspect ratio
+      //!=> contain: This scales the image uniformly so that both dimensions of the image will be equal to or less than the corresponding dimension of the view.
+      //!=> stretch: This is used to scale with height independently and may change the original image's aspect ratio.
+      //!=> repeat: The image is repeated to cover the frame of the view. 
+      //!=> center:  The image is centered in the view along with both dimensions.
    },
    container: {
       flex: 1,
@@ -19,5 +24,13 @@ export default StyleSheet.create({
       textAlign: 'center',
       fontSize: 20,
       fontWeight: 'bold'
+   },
+   image: {
+      height: 300,
+      width: 300,
+      resizeMode: 'cover',
+      borderWidth: 2,
+      borderColor: 'yellow',
+      borderRadius: 8
    }
 })
